4ebd6765125c5878605e876b12c1d2265b581058
Eugene Rodriguez <er11@usf.edu>
2025-01-25 18:48:12 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,10 @@
+// Task 1: Working with Arrays //
+let products = ["Health Potion", "Mana Potion", "Stamina Potion", "Antidote", "Elixir"];    // Array of products
+
+products.push("poison arrows");    // Add new product to the end of the array
+
+console.log(products);    // Print the array of products
+
+products.pop();    // Remove the last product from the array
+
+console.log("Updated products:",products);    // Print the array of products
